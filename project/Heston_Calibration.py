f136cbd4b97ce14143b6ab86e400e21352b512b1
Michał Jaworski <michal.jaworski@students.mimuw.edu.pl>
2017-12-18 13:08:29 +0100

    cleaning

--- project/Heston_Calibration.py
+++ project/Heston_Calibration.py
@@ -1,40 +1,42 @@
-# Equation (18) shows the characteristic function, which we are going to use in out project
 import cmath as cmath
 import math as math
 import numpy as np
 
+# SECTION 1 - Functions, which are necessary to compute price of the european option for given parameters.
+# Equation (18) p. 9 shows the characteristic function, which we are going to use in out project
 def ksi(theta, u):
-    return theta[3]-theta[4]*theta[2]*u*1j
+    return theta[3] - theta[4] * theta[2] * u * 1j
 def d(theta, u):
-    return cmath.sqrt(ksi(theta,u)*ksi(theta,u)+math.pow(theta[4],2)*u*u+u*1j)
+    return cmath.sqrt(ksi(theta, u)*ksi(theta, u) + math.pow(theta[4], 2)*u*u + u*1j)
 def g1(theta, u):
-    return (ksi(theta,u)+d(theta,u))/(ksi(theta,u)-d(theta,u))
+    return (ksi(theta, u) + d(theta, u))/(ksi(theta, u) - d(theta, u))
 def A1(theta, u, t):
-    return (u*u+1j*u)*cmath.sinh(d(theta,u)*t/2)
+    return (u*u + 1j*u)*cmath.sinh(d(theta, u)*t/2)
 def A2(theta, u, t):
-    return d(theta,u)*cmath.cosh(d(theta,u)*t/2)+ksi(theta,u)*cmath.sinh(d(theta,u)*t/2)
+    return d(theta, u)*cmath.cosh(d(theta, u)*t/2) + ksi(theta, u)*cmath.sinh(d(theta, u)*t/2)
 def A(theta, u, t):
-    return A1(theta,u,t)/A2(theta,u,t)
+    return A1(theta, u, t)/A2(theta, u, t)
 def B(theta, u, t):
-    return d(theta,u)*cmath.exp(theta[3]*t/2)/A2(theta,u,t)
+    return d(theta, u)*cmath.exp(theta[3]*t/2)/A2(theta, u, t)
 def D(theta, u, t):
-    return cmath.log(d(theta,u))+theta[3]*t/2-cmath.log(A2(theta,u,t))
+    return cmath.log(d(theta, u)) + theta[3]*t/2 - cmath.log(A2(theta, u, t))
 def char_function(theta, u, t, S_0, r):
-    return cmath.exp(1j*u*(math.log(S_0)+r*t)-t*theta[3]*theta[1]*theta[2]*1j*u/theta[4]-theta[0]*A(theta,u,t)+2*theta[3]*theta[1]*D(theta,u,t)/math.pow(theta[4],2))
+    return cmath.exp(1j*u*(math.log(S_0) + r*t) - t*theta[3]*theta[1]*theta[2]*1j*u/theta[4] - theta[0]*A(theta, u, t) + \
+                     2*theta[3]*theta[1]*D(theta, u, t)/math.pow(theta[4], 2))
 def integrate_char_function(theta, K, t, S_0, r, i, M, n, rule):
     delta_t = M/n
     if rule == "Riemann":
         value = 0
         for j in range(1, n+1):
-            value = value + cmath.exp(-1j*j*delta_t*math.log(K))/(1j*j*delta_t)*char_function(theta,j*delta_t-i,t,S_0,r)
+            value = value + cmath.exp(-1j*j*delta_t*math.log(K))/(1j*j*delta_t)*char_function(theta, j*delta_t-i, t, S_0, r)
         value = value*delta_t
     elif rule == "Simpson":
-        value = cmath.exp(-1j*delta_t*math.log(K))/(1j*delta_t)*char_function(theta,delta_t-i,t,S_0,r)
+        value = cmath.exp(-1j*delta_t*math.log(K))/(1j*delta_t)*char_function(theta, delta_t-i, t, S_0, r)
         for j in range(2, n, 2):
-            value = value + 4*cmath.exp(-1j*j*delta_t*math.log(K))/(1j*j*delta_t)*char_function(theta,j*delta_t-i,t,S_0,r)
+            value = value + 4*cmath.exp(-1j*j*delta_t*math.log(K))/(1j*j*delta_t)*char_function(theta, j*delta_t - i, t, S_0, r)
         for j in range(3, n-1, 2):
-            value = value + 2*cmath.exp(-1j*j*delta_t*math.log(K))/(1j*j*delta_t)*char_function(theta,j*delta_t-i,t,S_0,r)
-        value = value + cmath.exp(-1j*M*math.log(K))/(1j*M)*char_function(theta,M-i,t,S_0,r)
+            value = value + 2*cmath.exp(-1j*j*delta_t*math.log(K))/(1j*j*delta_t)*char_function(theta, j*delta_t - i, t, S_0, r)
+        value = value + cmath.exp(-1j*M*math.log(K))/(1j*M)*char_function(theta, M - i, t, S_0, r)
         value = value*delta_t/3
     elif rule == "Gauss-Legendre":
         value = 0
@@ -42,30 +44,33 @@ def integrate_char_function(theta, K, t, S_0, r, i, M, n, rule):
         #f = lambda x: cmath.exp(-1j*x*math.log(K))/(1j*x)*char_function(theta,x-i,t,S_0,r)
         x, w = np.polynomial.legendre.leggauss(deg)
         for j in range(0, deg): # Tutaj siedzie deg
-            u = (x[j]+1)*0.5*M
-            value = value + w[j]*cmath.exp(-1j*u*math.log(K))/(1j*u)*char_function(theta,u-i,t,S_0,r)
+            u = (x[j] + 1)*0.5*M
+            value = value + w[j]*cmath.exp(-1j*u*math.log(K))/(1j*u)*char_function(theta, u - i, t, S_0, r)
         value = value*0.5*M
     return value.real
 def integrate_char_function_GL(theta, K, t, S_0, r, i, M, deg):
     x, w = np.polynomial.legendre.leggauss(deg)
     u = (x[0]+1)*0.5*M
-    value = w[0]*cmath.exp(-1j*u*math.log(K))/(1j*u)*char_function(theta,u-i,t,S_0,r)
+    value = w[0]*cmath.exp(-1j*u*math.log(K))/(1j*u)*char_function(theta, u - i, t, S_0, r)
     for j in range(1, deg): # Tutaj siedzi deg
-        u = (x[j]+1)*0.5*M # Nie musialobyc tej linijki, tutaj jest jakis problem z formatem
-        value = value + w[j]*cmath.exp(-1j*u*math.log(K))/(1j*u)*char_function(theta,u-i,t,S_0,r)
+        u = (x[j] + 1)*0.5*M # Nie musialobyc tej linijki, tutaj jest jakis problem z formatem
+        value = value + w[j]*cmath.exp(-1j*u*math.log(K))/(1j*u)*char_function(theta, u - i, t, S_0, r)
     value = value*0.5*M
     return value.real
 def eur_call_heston_price(theta, K, t, S_0, r, M, n, rule):
-    return (S_0-math.exp(-r*t)*K)/2+math.exp(-r*t)/math.pi*(integrate_char_function(theta,K,t,S_0,r,1j,M,n,rule)-K*integrate_char_function(theta,K,t,S_0,r,0,M,n,rule))
+    return (S_0 - math.exp(-r*t)*K)/2 + math.exp(-r*t)/math.pi*(integrate_char_function(theta, K, t, S_0, r, 1j, M, n, rule) - \
+                                                                K*integrate_char_function(theta, K, t, S_0, r, 0, M, n, rule))
 def eur_call_heston_price_GL(theta, K, t, S_0, r, M, deg):
-    return (S_0-math.exp(-r*t)*K)/2+math.exp(-r*t)/math.pi*(integrate_char_function_GL(theta,K,t,S_0,r,1j,M,deg)-K*integrate_char_function_GL(theta,K,t,S_0,r,0,M,deg))
-
+    return (S_0 - math.exp(-r*t)*K)/2 + math.exp(-r*t)/math.pi*(integrate_char_function_GL(theta, K, t, S_0, r, 1j, M, deg) - \
+                                                                K*integrate_char_function_GL(theta, K, t, S_0, r, 0, M, deg))
 #theta = np.array([0.09, 0.09, -0.3, 2, 1])
 #print(eur_call_heston_price(theta, 100, 5, 100, 0.05, 10, 10000, "Riemann"))
 #print(eur_call_heston_price(theta, 100, 5, 100, 0.05, 10, 10000, "Simpson"))
 #print(eur_call_heston_price(theta, 100, 5, 100, 0.05, 10, 10000, "Gauss-Legendre"))
 #print(eur_call_heston_price_GL(theta, 100, 5, 100, 0.05, 10, 10))
 
+# SECTION 2 - Functions, which are necessary to compute gradient of the characteristic function.
+# Equation (22) p. 11 shows the gradient of the characteristic function, which we are going to use in out project
 def h_1(theta, u, t):
     return -A(theta,u,t)
 def h_2(theta, u ,t):
