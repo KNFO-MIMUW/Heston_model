d41addfb99c4381a9200fdf9693cea62b6b03464
Michał Jaworski <michal.jaworski@students.mimuw.edu.pl>
2017-12-24 15:16:20 +0100

    main file

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
 import heston_functions as hf
-import numpy as np
-import pandas as pd
-from scipy import optimize
+from uploading_data import theta_0, data
+from option_class import option
 
-#print(optimize.least_squares(hf.r_function, theta_0, method='lm').x)
+theta = hf.calibration_heston(theta_0)
+option_1 = option(theta, data)
+print(option_1.eur_option())
+print(option_1.asian_option())
