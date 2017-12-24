f6bfa63e5a8605cce8ed73b4414d701407b4d849
Michał Jaworski <michal.jaworski@students.mimuw.edu.pl>
2017-12-24 15:15:06 +0100

    data - py file

--- project/data.py
+++ project/data.py
@@ -2,8 +2,8 @@ import numpy as np
 import heston_functions as hf
 import pandas as pd
 
-theta_0 = np.array([0.07, 0.11, -0.75, 3.15, 0.15])
-theta = np.array([0.08, 0.10, -0.8, 3, 0.25])
+theta = np.array([0.07, 0.11, -0.75, 3.15, 0.15])
+theta_0 = np.array([0.08, 0.10, -0.8, 3, 0.25])
 data = np.array([100, 0.02, 100, 64]) # S_0, r, M, deg
 S_0 = 100
 r = 0.02
@@ -30,13 +30,10 @@ mkt_data = np.vstack(([hf.eur_call_heston_price(theta, 80, 0.25, data[0], data[1
                       [hf.eur_call_heston_price(theta, 120, 1, data[0], data[1], data[2], 64), 120, 1],
                       [hf.eur_call_heston_price(theta, 120, 2, data[0], data[1], data[2], 64), 120, 2],
                       ))
-
 mkt_data = pd.DataFrame(mkt_data)
 mkt_data.columns = ['price', 'K', 'T']
 pd.DataFrame(mkt_data).to_csv('mkt_data.csv', index=False, sep=',', decimal='.')
-theta = pd.DataFrame({'parameter': ['v_0', 'bar_v', 'rho', 'kappa', 'sigma'], 'value': theta})
-pd.DataFrame(theta).to_csv('theta.csv', index=False, sep='=', decimal='.')
 theta_0 = pd.DataFrame({'parameter': ['v_0', 'bar_v', 'rho', 'kappa', 'sigma'], 'value': theta_0})
-pd.DataFrame(theta).to_csv('theta_0.csv', index=False, sep='=', decimal='.')
+pd.DataFrame(theta_0).to_csv('theta_0.csv', index=False, sep='=', decimal='.')
 data = pd.DataFrame({'parameter': ['S_0', 'r', 'M', 'deg'], 'value': data})
 pd.DataFrame(data).to_csv('additional_data.csv', index=False, sep='=', decimal='.')
